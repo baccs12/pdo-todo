ce2981b2a76ce62163f3d874050cecdd915192ce
Baccs <revlv.jbaccs@gmail.com>
2016-06-13 12:08:33 +0800

    Join Displaying and Posting in 1 page

--- db.php
+++ db.php
@@ -14,7 +14,7 @@
     Last Name: <br>
     <input type='text' name='lastname' required='required' ><br>
     Email: <br>
-     <input type='email' name='email' id='email' required='required' ><br><br>
+    <input type='email' name='email' id='email' required='required' ><br><br>
     <input type='submit' name='submit' value='submit'>
 
     </form>
@@ -46,6 +46,14 @@ try {
     }
 }
 
+
+$sql = $link->query('SELECT * FROM MyGuests');
+$results = $sql->fetchAll(PDO::FETCH_ASSOC);
+
+foreach($results as $result){
+echo "<table border = '1px'>";
+echo "<tr><td>" . $result[id] . "</td><td>" . $result["firstname"] . "</td><td>" . $result["lastname"] . "</td><td>" . $result["email"] . "</td><td>" . "<span><a href='#'>Delete</a></span>" . "<span><a href='#'>Update</a></span>" . "</td></tr>";
+}
 
 
 ?>
