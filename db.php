e83674cd57230e8975679db8d6c827a663d8c33d
Baccs <revlv.jbaccs@gmail.com>
2016-06-14 14:00:31 +0800

    Done Class add

--- db.php
+++ db.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -9,12 +10,11 @@
 <body>
 <div class='wrapper'>
     <form name='baccs' method='post' action='db.php'>
-    First Name: <br>
-    <input type='text' name='firstname' required='required' ><br>
-    Last Name: <br>
-    <input type='text' name='lastname' required='required' ><br>
-    Email: <br>
-    <input type='email' name='email' id='email' required='required' ><br><br>
+    Title: <br>
+    <input type='text' name='title' required='required' ><br>
+    Message: <br>
+    <input type='text' name='message' required='required' ><br>
+    <br>
     <input type='submit' name='submit' value='submit'>
 
     </form>
@@ -23,48 +23,55 @@
 
 
 <?php
-if(isset($_POST["submit"])){
-try {
-    $link = new PDO('mysql:host=localhost;dbname=baccs', "root", "P@ssw0rd");
-    $link->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $sql="INSERT INTO `baccs` . `MyGuests` (`firstname`, `lastname`, `email`)
-    VALUES ('$_POST[firstname]','$_POST[lastname]','$_POST[email]')";
-    if ($link->query($sql)) {
-    echo "<script type= 'text/javascript'>alert('New Record Inserted Successfully');</script>";
-    }
-    else{
-    echo "<script type= 'text/javascript'>alert('Data not successfully Inserted.');</script>";
-    }
-    $dbh = null;
-    }
-    catch(PDOException $e)
+class Pdoclass
+{
+    public function __construct($db)
     {
-    echo $e->getMessage();
+
     }
-}
 
+    public function addsubmit($result) {
 
-
-?>
-<?php
-$sql= "SELECT * FROM MyGuests";
-        $stmt = $link->prepare($sql);
-        $stmt->execute();
-        $result = $stmt->fetchAll();
-        foreach($result as $row)
-        {
-            echo "<div class='wrapper'>
-            <form action='delete.php' method='POST' class='form-display'>";
-            echo "<input type='text' name='id' value='{$row['id']}'>";
-            echo "<input type='text' name='firstname' value='{$row['firstname']}'>";
-            echo "<input type='text' name='lastname' value='{$row['lastname']}'>";
-            echo "<input type='text' name='email' value='{$row['email']}'>";
-            echo "<button type='submit' name='update'>Update</button>";
-            echo "<button type='submit' name='delete'>Delete</button>";
-            echo "</form></div>";
+    if(isset($_POST["submit"])){
+    try {
+        $link = new PDO('mysql:host=localhost;dbname=baccs', "root", "P@ssw0rd");
+        $link->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $sql="INSERT INTO `baccs` . `ToDo` (`title`, `message`)
+        VALUES ('$_POST[title]','$_POST[message]')";
+        if ($link->query($sql)) {
+        echo "<script type= 'text/javascript'>alert('New Record Inserted Successfully');</script>";
         }
-?>
-</form>
+        else{
+        echo "<script type= 'text/javascript'>alert('Data not successfully Inserted.');</script>";
+        }
+        $dbh = null;
+        }
+        catch(PDOException $e)
+        {
+        echo $e->getMessage();
+        }
 
-</body>
-</html>
+        $sql= "SELECT * FROM ToDo";
+                $stmt = $link->prepare($sql);
+                $stmt->execute();
+                $result = $stmt->fetchAll();
+                foreach($result as $row)
+                {
+                    echo "<div class='wrapper'>
+                    <form action='delete.php' method='POST' class='form-display'>";
+                    echo "<input type='text' name='id' value='{$row['id']}'>";
+                    echo "<input type='text' name='firstname' value='{$row['title']}'>";
+                    echo "<input type='text' name='lastname' value='{$row['message']}'>";
+                    echo "<button type='submit' name='update'>Update</button>";
+                    echo "<button type='submit' name='delete'>Delete</button>";
+                    echo "</form></div>";
+                }
+            }
+        }
+    }
+
+$info = new Pdoclass($db);
+$info->addsubmit($result);
+
+
+?>
--- update.php
+++ update.php
@@ -3,4 +3,19 @@
 $link = new PDO('mysql:host=localhost;dbname=baccs', "root", "P@ssw0rd");
 $link->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
+if(isset($_POST['update'])){
+    $sql = "UPDATE MyGuests
+            SET id = :id,
+                firstname = :firstname,
+                lastname = :lastname,
+                email = :email,
+            WHERE id = :id";
+    $stmt = $link->prepare($sql);
+    $stmt->execute(array(":id" => $_POST['updateid'],
+                    ":firstname" => $_POST['firstname'],
+                    ":lastname" => $_POST['lastname']
+                    ":email" => $_POST['email']));
+}
 
+// header('Location: db.php');
+// exit();
